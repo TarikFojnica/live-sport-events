3507ab34359f023e135f7e90edf00431c5345007
tarikfojnica <tarik.f786@gmail.com>
2017-04-22 13:01:48 +0200

    Ad helpers

--- src/components/sections/Layout.js
+++ src/components/sections/Layout.js
@@ -5,9 +5,10 @@ class Layout extends React.Component {
 	render() {
 		return (
 			<div className="app-container">
-
-				<div className="page">
-					{this.props.children}
+				<div className="container">
+					<div className="page">
+						{this.props.children}
+					</div>
 				</div>
 			</div>
 		);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './components/AppRoutes'
-import './index.css';
+import './assets/styles/main.css';
 
 ReactDOM.render(
 	<AppRoutes />,
