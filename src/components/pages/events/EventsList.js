fcfeda52eb572796ed75ba9621d6d093e7c07a63
tarikfojnica <tarik.f786@gmail.com>
2017-05-18 19:11:05 +0200

    Add events list page

---
+++ src/components/pages/events/EventsList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class EventsList extends React.Component {
+
+	render() {
+		return (
+			<div className="page events-list">
+
+			</div>
+		);
+	}
+}
+
+export default EventsList;
---
+++ src/components/pages/events/EventsListElement.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class EventsList extends React.Component {
+
+	render() {
+		return (
+			<div className="events-list-element">
+
+			</div>
+		);
+	}
+}
+
+export default EventsList;
